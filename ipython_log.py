58853ef23f8476bb1671867649ca881c2529716a
PJFinnerty <finnertypete@gmail.com>
2020-03-29 18:05:36 +0100

    Added some ipython logs, these to be moved to another folder in due course. 'w8plots' modified.

--- ipython_log.py
+++ ipython_log.py
@@ -1,6 +1,51 @@
 # IPython log file
 
+import numpy as np
+import matplotlib.pyplot as plt
 get_ipython().run_line_magic('logstart', '')
-import pandas as pd
-pd.read_txt("http://www.gutenberg.org/files/2701/2701-0.txt")
+def f(x): (x, 4)
+def g(x): (x*x, 4)
+def h(x): (x**3, 4)
+x = 4
+plt.plot(f(x))
+x = np.arange(0.0, 4.0, 0.2)
+plt.plot(x)
+x1 = (x, 4)
+x2 = (x*x, 4)
+x3 = (x**3, 4)
+y = 4
+plt.plot(x1, y)
+x = np.linspace(0.0, 4.0, 0.5)
+plt.plot(x1, y)
+x = 2
+a = x
+b = x*x
+c = x**3
+plt.plot(a, 4)
+plt.plot(a, 4, 'g.')
+plt.plot(b, 4, 'r.')
+plt.plot(c, 'b.')
+x = np.arange(0.0,4.0,0.5)  def f(x):     (x, 4)  def g(x):     (x*x, 4)  def h(x):     (x**3, 4)  plt.plot(x, 4)
+x = np.arange(0.0,4.0,0.5)  def f(x):     (x, 4)  def g(x):     (x*x, 4)  def h(x):     (x**3, 4) 
+x = 2
+def f(x): (x, 4)
+def h(x): (x*x, 4)
+def g(x): (x**3, 4)
+f
+plt.plot(f(x))
+plt.plot(x)
+y = def f(x): (x, 4)
+def f(x) = x
+def f(x): x
+x = 2
+def f(x):x
+def g(x): x*x
+def h(x): x**3
+rangee = np.range(0, 4, 0.2)
+rangee = np.arange(0, 4, 0.2)
+f(x): rangee
+f(x)
+plt.plotf(x)
+f(x) = x
+f(x) = 2
 exit()
---
+++ w8plots.py
@@ -0,0 +1,41 @@
+#Peter Finnerty - Week 8 Task
+
+#Write a program that displays a plot of the functions 
+# f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] 
+# on the one set of axes
+
+#Import numpy for arrays and matplotlib for plotting.
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Create a variable 'x' and assign the range.
+x = np.arange(0.0, 4.0)
+
+#Create the 3 funcions and substiute 'x' in.
+def f(x): 
+    return x
+
+def g(x):
+    return x * x
+
+def h(x):
+    return x **3
+
+#Create  representation of the 3 functions, using the chosen
+#label and linewidth formats.
+plt.plot( f(x), 'b', label = 'f(x)', linewidth = 1.5)
+plt.plot( g(x), 'r', label = 'g(x)', linewidth = 1.5) 
+plt.plot( h(x),'g', label = 'h(x)', linewidth = 1.5)
+
+#Add further plot details including line grids and legends.
+#Information on specialised formatting from Edureka:
+# @ https://www.youtube.com/watch?v=yZTBMMdPOww&list=PL9ooVrP1hQOHY-BeYrKHDrHKphsJOyRyu&index=68&t=0s
+plt.title('Week 8 Task')
+plt.ylabel('Y-Axis')
+plt.xlabel('X-Axis')
+plt.legend()
+plt.grid(True, color = 'k')
+
+#Use the show() function to ensure that the graph is always
+#shown when the program is run.
+plt.show()
